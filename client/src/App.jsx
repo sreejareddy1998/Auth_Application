f47bd82f0da3a1d9fda26c045115c0df0e1fd32a
sreeja <sreejamadireddy1998@gmail.com>
2024-05-04 07:50:07 -0400

    Adding Routes and creating components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Profile from "./components/Profile";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
