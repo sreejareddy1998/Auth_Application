cd22effa48931a6746f458b8135ccab10582aa99
sreeja <sreejamadireddy1998@gmail.com>
2024-05-10 18:49:06 -0400

    connected to mongodb, installed mongoose and  dotenv

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch(
+    (error) => console.error("Error connecting to database:", error)
+    // console.log(process.env.MONGO)
+  );
 const app = express();
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
