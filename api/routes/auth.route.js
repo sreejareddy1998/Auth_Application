bcf93ae93c436905f04a4ce0e18767114d558f5b
sreeja <sreejamadireddy1998@gmail.com>
2024-05-19 00:49:55 -0400

    installed jsonwebtoken , signIn backend and UI

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 const router = Router();
 router.post("/signup", signup);
+router.post("/signin", signin);
 export default router;
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,7 +1,70 @@
-import React from "react";
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-3xl font-semibold my-7">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 p-3 rounded-lg uppercase text-white hover:opacity-95 disabled:opacity-80">
+          {loading ? "Loading....." : "SignIn"}
+        </button>
+      </form>
+      <div className="flex mt-5 gap-2">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <div className="text-red-700 mt-5">{error && "Something went wrong"}</div>
+    </div>
+  );
 };
 
 export default SignIn;
